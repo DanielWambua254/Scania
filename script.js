9f00a559bec5a69a0743462b0a6767efc7361c5f
BMW <wambuadaniel2001@gmail.com>
2022-12-16 14:39:06 +0300

    Added files for this site

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuBtn  = document.querySelector(".menu-btn");
+const navMenu  = document.querySelector(".menu");
+
+// click listeners for small screen size menu. The virtical one 
+document.querySelectorAll(".menu").forEach(n => n.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+}))
+
+// works for three bars on top right when using small display screen
+function clickFunction() {
+    menuBtn.addEventListener("click", () =>{
+    navMenu.classList.toggle("active");
+})
+}
